7d3989f97d284ffff217272412999588079f5b15
Julien <julien@ripouteau.com>
2022-01-06 21:50:26 +0100

    ✨ added routes names + crop when overflow

--- commands/PrettyRoute.ts
+++ commands/PrettyRoute.ts
@@ -13,8 +13,14 @@ export default class PrettyRoutes extends BaseCommand {
     stayAlive: false,
   }
 
+  @flags.boolean({ alias: 'f', name: 'verbose', description: 'Display more information' })
+  public verbose: boolean = false
+
   @flags.boolean({ alias: 'r', name: 'reverse', description: 'Reverse routes display' })
   public reverse: boolean = false
+
+  @flags.boolean({ alias: 'j', name: 'json', description: 'Output to JSNO' })
+  public json: boolean = false
 
   @flags.string({ alias: 'm', name: 'method', description: 'Filter routes by method' })
   public methodFilter: string
@@ -25,6 +31,9 @@ export default class PrettyRoutes extends BaseCommand {
   @flags.string({ alias: 'n', name: 'name', description: 'Filter routes by name' })
   public nameFilter: string
 
+  /**
+   * The colors associated with each HTTP method
+   */
   protected methodColors = {
     ANY: this.colors.red.bind(this.colors),
     GET: this.colors.blue.bind(this.colors),
@@ -39,11 +48,25 @@ export default class PrettyRoutes extends BaseCommand {
   public async run() {
     const Router = this.application.container.use('Adonis/Core/Route')
 
+    /**
+     * Commit routes before we can read them
+     */
     Router.commit()
 
-    // TODO : add domains
+    if (this.json) {
+      this.log(JSON.stringify(this.routerToJson(Router), null, 2))
+    } else {
+      this.outputList(Router)
+    }
+  }
 
-    let routes = this.toJson(Router).root
+  private getTerminalWidth() {
+    return process.stdout.columns || 80
+  }
+
+  private outputList(router: RouterContract) {
+    // TODO: add domains
+    let routes = this.routerToJson(router).root
     const termWidth = this.getTerminalWidth()
 
     if (this.reverse) routes = routes.reverse()
@@ -52,45 +75,80 @@ export default class PrettyRoutes extends BaseCommand {
     }
 
     const maxMethodsLength = Math.max(...routes.map((route) => route.methods.join('|').length)) - 1
+
     routes.forEach((route) => {
-      const methodsLength = route.methods.join('|').length
-      const methodsOutput = route.methods
-        .map((method) => this.methodColors[method.toUpperCase()](method))
-        .join('|')
+      const methods = route.methods.join('|')
 
-      const spaces = ' '.repeat(Math.max(maxMethodsLength + 5 - methodsLength, 0))
-      const patternOutput = route.pattern.replace(/\/(:.*?)\//g, `/${this.colors.yellow('$1')}/`)
+      /**
+       * Spaces needed to align the start of route patterns
+       */
+      const spaces = ' '.repeat(Math.max(maxMethodsLength + 5 - methods.length, 0))
 
-      const dots = '.'.repeat(
-        Math.max(
-          termWidth -
-            methodsLength -
-            spaces.length -
-            route.pattern.length -
-            route.name.length -
-            route.handler.length -
-            5,
-          0
-        )
-      )
+      let nameAndHandler = route.name ? ` ${route.name} ⇒ ${route.handler}` : ` ${route.handler}`
+      const totalLength = (methods + spaces + route.pattern + ' ' + nameAndHandler).length
 
-      console.log(
-        methodsOutput +
-          spaces +
-          patternOutput +
-          ` ${this.colors.grey(dots)} ` +
-          this.colors.grey(route.name) +
-          ' > ' +
-          route.handler
-      )
+      if (totalLength > termWidth) {
+        const lenWithoutNameAndHandler = (methods + spaces + route.pattern + ' ').length
+        nameAndHandler = nameAndHandler.substring(0, termWidth - lenWithoutNameAndHandler - 1) + '…'
+      }
+
+      /**
+       * How many dots we need to align the handlers
+       */
+      const dots = ' ' + '.'.repeat(Math.max(termWidth - totalLength, 0))
+
+      const middlewares = route.middleware
+        .map((middleware) => {
+          const startSpace = ' '.repeat(maxMethodsLength + 5)
+          return this.colors.grey(`${startSpace}⇂ ${middleware}`)
+        })
+        .join('\n')
+
+      this.outputRoute(route.methods, spaces, route.pattern, dots, nameAndHandler, middlewares)
     })
   }
 
-  private getTerminalWidth() {
-    return process.stdout.columns || 80
+  /**
+   * Output route and middlewares by concatenating and colorizing each part of it
+   */
+  private outputRoute(
+    methods: string[],
+    spaces: string,
+    pattern: string,
+    dots: string,
+    nameAndHandler: string,
+    middlewares: string
+  ) {
+    const methodsOutput = methods
+      .map((method) => this.methodColors[method.toUpperCase()](method))
+      .join('|')
+
+    const patternOutput = pattern.replace(/:([^/]+)/gm, `${this.colors.yellow('$&')}`)
+    const nameAndHandlerOutput = this.colors.grey(nameAndHandler)
+    const dotsOutput = this.colors.grey(dots)
+
+    this.log(methodsOutput + spaces + patternOutput + dotsOutput + nameAndHandlerOutput)
+
+    if (middlewares && this.verbose) {
+      this.log(middlewares)
+    }
   }
 
-  private toJson(router: RouterContract) {
+  /**
+   * Stolen code from @adonisjs/core/commands/ListRoutes.ts
+   */
+  private log(message: string) {
+    if (this.application.environment === 'test') {
+      this.logger.log(message)
+    } else {
+      console.log(message)
+    }
+  }
+
+  /**
+   * Stolen code from @adonisjs/core/commands/ListRoutes.ts
+   */
+  private routerToJson(router: RouterContract) {
     const routes = router.toJSON()
 
     return Object.keys(routes).reduce<{
